607fe385d5fea8a505ac13f3b7b7d77a24dc99c9
pommevilla <pvillanueva13@gmail.com>
2018-09-18 00:52:44 -0500

    File reorganization

--- notebooks/sections/affine_shift.m
+++
@@ -1,6 +0,0 @@
-function [ aff_shift ] = affine_shift(x, M, b)
-%UNTITLED5 Summary of this function goes here
-%   Detailed explanation goes here
-aff_shift = M * x + b;
-end
-
--- notebooks/sections/square.m
+++
@@ -1,5 +0,0 @@
-function [ x_sqr ] = square(x)
-%square: Calculate the square of input
-%    Returns the input multiplied by itself.  
-    x_sqr = x * x;
-end
