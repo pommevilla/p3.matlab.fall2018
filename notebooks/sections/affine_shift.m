42058280cb0c25f53e65a6310b38b5ae65f98ea4
pommevilla <pvillanueva13@gmail.com>
2018-09-17 20:13:51 -0500

    Finish Sections 2.1, 2.2

---
+++ notebooks/sections/affine_shift.m
@@ -0,0 +1,6 @@
+function [ aff_shift ] = affine_shift(x, M, b)
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+aff_shift = M * x + b;
+end
+
---
+++ notebooks/sections/square.m
@@ -0,0 +1,5 @@
+function [ x_sqr ] = square(x)
+%square: Calculate the square of input
+%    Returns the input multiplied by itself.  
+    x_sqr = x * x;
+end
